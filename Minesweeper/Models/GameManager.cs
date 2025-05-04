c498f16ca8d2ebe28ce4ccc7f5c9ed807d605b91
VadimSushinskiy <vadim0sushinsky@gmail.com>
2025-05-04 16:14:57 +0300

    Add Initialize and ActivateCell methods

---
+++ Minesweeper/Models/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Minesweeper.Models.Field;
+
+namespace Minesweeper.Models
+{
+    public class GameManager
+    {
+        public Field.Field Field { get; set; }
+
+        public int Rows { get; set; }
+
+        public int Score { get; set; }
+
+        public int Columns { get; set; }
+
+        public bool IsEnd { get; set; }
+
+        public bool IsWin { get; set; }
+
+        public void Initialize(int rows, int columns, string difficulty)
+        {
+            Field = new Field.Field(rows, columns, difficulty);
+            IsEnd = false;
+            IsWin = false;
+        }
+    }
+}
