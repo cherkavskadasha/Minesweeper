ab9dce9b77291157d37000ea5a2730bd258c7e1e
VadimSushinskiy <vadim0sushinsky@gmail.com>
2025-05-04 15:15:31 +0300

    Add CreateField algorithm

---
+++ Minesweeper/Models/Field/Cell.cs
@@ -0,0 +1,24 @@
+﻿namespace Minesweeper.Models.Field
+{
+    public class Cell
+    {
+        public bool IsBomb { get; set; }
+
+        public bool IsActivated { get; set; }
+
+        public CellType CellType { get; set; }
+
+        public Cell(bool isBomb, CellType cellType)
+        {
+            IsBomb = isBomb;
+            IsActivated = false;
+            CellType = cellType;
+        }
+
+        public bool Activate()
+        {
+            IsActivated = true;
+            return IsBomb;
+        }
+    }
+}
---
+++ Minesweeper/Models/Field/Field.cs
@@ -0,0 +1,12 @@
+﻿namespace Minesweeper.Models.Field
+{
+    public class Field
+    {
+        public Cell[,] Cells { get; set; }
+
+        public Field()
+        {
+
+        }
+    }
+}
